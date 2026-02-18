3413215f6a6f67b8491fc2d75010aa930d2ed25e
rakshith_s <rakshithsantosh@gmail.com>
2026-02-18 13:49:28 +0530

    studio fixes

--- web/src/app/studio/[[...index]]/page.tsx
+++ web/src/app/studio/[[...index]]/page.tsx
@@ -1,16 +1,8 @@
 "use client"
 
-// import { NextStudio } from "next-sanity/studio";
-// import config from "../../../../sanity.config";
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../sanity.config";
 
 export default function StudioPage() {
-    return (
-        <div className="flex items-center justify-center h-screen bg-slate-50">
-            <div className="text-center">
-                <h1 className="text-2xl font-bold text-slate-900">Sanity Studio</h1>
-                <p className="text-slate-600">Please run the project locally to access the Studio.</p>
-            </div>
-        </div>
-    );
-    // return <NextStudio config={config} />;
+    return <NextStudio config={config} />;
 }
